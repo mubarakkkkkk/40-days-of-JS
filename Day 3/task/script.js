d33795119a14509b5d9c04ac1d1aff4f9044016e
Mubarakkkkk K <abiolamubarakolamide05@gmail.com>
2026-01-06 22:30:44 +0100

    feay

---
+++ Day 3/task/script.js
@@ -0,0 +1,25 @@
+const number = 7;
+const result = number % 2;
+
+console.log(number);
+console.log(result);
+result === 0 ? console.log("even") : console.log("odd");
+
+// Drivers Licence Age Checker
+const age = 10;
+age >= 18 ?
+console.log("You are old enough to drive") : console.log("You are not old enough to drive");
+
+// CTC Calculator
+const salary = 12300;
+
+annualSalary = salary * 12;
+console.log(annualSalary);
+bonus = annualSalary * 0.2;
+console.log(bonus)
+CTC = annualSalary + bonus;
+console.log(CTC);
+
+// Traffic Light Simulation
+const color = "green";
+ color === "green" ? console.log("Go") : color === "red" ? console.log("stop") : console.log("invalid")
